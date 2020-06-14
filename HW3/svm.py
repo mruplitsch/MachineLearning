35384001546f6c9ceaf8807509804141ef4e6b61
Markus Ruplitsch <markus.ruplitsch@student.tugraz.at>
2020-06-14 19:30:42 +0200

    added calculation of margin and changed support vector lines

--- HW3/svm.py
+++ HW3/svm.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+Iterations = 500
+
 
 # simple dataset
 def createDataset_1():
@@ -22,7 +24,7 @@ def createDataset_1():
         X.append(np.random.multivariate_normal(mu_2, Sigma_2))
         t.append(-1)
 
-    return np.asarray(X), np.asarray(t).reshape(N, 1)
+    return np.asarray(X), np.reshape(t, (N, 1))
 
 
 # half-moon dataset
@@ -54,32 +56,141 @@ def createDataset_2():
 
     X = np.append(X_1, X_2, 0)
     t = np.append(t_1, t_2, 0)
-    return np.asarray(X), np.asarray(t).reshape(N, 1)
+    return np.asarray(X), np.reshape(t, (N, 1))
+
+
+# phi function from Assignment Sheet
+def phi(x):
+    return np.append(1, x)
+
+
+# g_i function from Assignment Sheet
+def g_i(w_tilde, xn, tn):
+    if tn * np.inner(w_tilde, phi(xn)) >= 1:
+        return 0
+    else:
+        return -tn * phi(xn)
+
+
+# algorithm 1 from Assignment Sheet
+def proximalSubGradientMethod(X, t):
+    N = np.shape(t)[0]
+    gamma_1 = 10 ** (-6)
+    gamma = np.array(([gamma_1, 1.0, 1.0]))
+    w_tilde = [np.array(([1, 1, 1]))]
+    alpha = 0.01
+    lambda_ = 0.01
+
+    for j in range(0, Iterations):
+        if(j % 50 == 0):
+            print("Iteration " + str(j) + "/" + str(Iterations))
+        gs = []
+        for i, xn, tn in zip(range(0, N), X, t[:, 0]):
+            gs.append(g_i(w_tilde[-1], xn, tn))
+            g = np.sum(gs) / (i + 1)
+
+            next_w_tilde = (w_tilde[-1] - alpha * g) / (1 + alpha * lambda_ * gamma)
+            w_tilde.append(next_w_tilde)
+    return w_tilde[-1]
+
+
+# calculates classification accuracy
+def accuracy(X, t, w_tilde):
+    N = np.shape(X)[0]
+    correct = 0
+    for xn, tn in zip(X, t):
+        y = np.inner(w_tilde, phi(xn))
+        if(y >= 0 and tn == +1):
+            correct += 1
+        elif (y < 0 and tn == -1):
+            correct += 1
+    return correct / N
+
+
+# finds data points closest to separating hyperplane
+# and calculates y offset from hyperplane
+def supportVector(X, t, w_tilde, k, d):
+    N = np.shape(X)[0]
+    indices = []
+    offset = 0
+    min = np.abs(np.inner(w_tilde, phi(X[0])))
+    margin = np.abs(w_tilde[0] + d * w_tilde[2]) / np.sqrt((w_tilde[1] ** 2) + (w_tilde[2] ** 2))
+    for i, xn, tn in zip(range(0, N), X, t):
+        y = np.abs(np.inner(w_tilde, phi(xn)))
+        if y < min:
+            indices = [i]
+            offset = k * xn[0] + d - xn[1]
+
+            min = y
+            d_2 = d - offset
+            margin = np.abs((d - offset) * w_tilde[2] + w_tilde[0]) / np.sqrt((w_tilde[1] ** 2) + (w_tilde[2] ** 2))
+        elif y == min:
+            indices.append(i)
+
+    return indices, offset, margin
 
 
 # creates scatter plot from data
-def plotData(X, t):
+def plotData(X, t, w_tilde=[]):
+    plt.title("Proximal Sub-gradient Method with " + str(Iterations) + " Iterations")
 
     group_1 = X[np.where(t == 1)[0]]
     group_2 = X[np.where(t == -1)[0]]
 
-    plt.scatter(group_1[:, 0], group_1[:, 1], label="t=1")
-    plt.scatter(group_2[:, 0], group_2[:, 1], label="t=2")
+    plt.scatter(group_1[:, 0], group_1[:, 1], label="t = +1")
+    plt.scatter(group_2[:, 0], group_2[:, 1], label="t = -1")
+
+    if len(w_tilde):
+        d = w_tilde[0] / -w_tilde[2]
+        k = w_tilde[1] / -w_tilde[2]
+        x = np.arange(-3, 10, 0.1)
+        y = k * x + d
+        plt.plot(x, y, 'C3', label="Separating Hyperplane")
+
+        indices, offset, margin = supportVector(X, t, w_tilde, k, d)
+        plt.plot(x, k * x + d - offset, 'C5', label="Support Vectors")
+        plt.plot(x, k * x + d + offset, 'C5')
+        plt.scatter(X[indices, 0], X[indices, 1])
+        plt.title("Proximal Sub-gradient Method with " + str(Iterations) + " Iterations, Margin = {:.2f}".format(margin))
+
+    xmin = np.min(X[:, 0]) - 1
+    ymin = np.min(X[:, 1]) - 1
+    xmax = np.max(X[:, 0]) + 1
+    ymax = np.max(X[:, 1]) + 1
+    plt.xlim(xmin, xmax)
+    plt.ylim(ymin, ymax)
+
     plt.legend()
     plt.show()
 
 
 
 def main():
+    # Task 1
     # simple dataset
     X_1, t_1 = createDataset_1()
 
     # half-moon dataset
     X_2, t_2 = createDataset_2()
 
+    # Task 2
+    result_1 = proximalSubGradientMethod(X_1, t_1)
+    #result_2 = proximalSubGradientMethod(X_2, t_2)
+
+    accuracy_1 = accuracy(X_1, t_1, result_1)
+    #accuracy_2 = accuracy(X_2, t_2, result_2)
+
+    print("Accuracies:")
+    print(accuracy_1)
+    #print(accuracy_2)
+
     # plots
     plotData(X_1, t_1)
-    plotData(X_2, t_2)
+    #plotData(X_2, t_2)
+
+    # plots with decision boundary
+    plotData(X_1, t_1, result_1)
+    #plotData(X_2, t_2, result_2)
 
 
 
